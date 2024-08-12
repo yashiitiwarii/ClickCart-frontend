00c9c5f10472c6d701f8eaa9a4c2e220ef32bd4b
yashiitiwarii <101939001+yashiitiwarii@users.noreply.github.com>
2024-08-12 15:46:28 +0530

    removed backend

--- src/Components/LinkWithIcon.jsx
+++ src/Components/LinkWithIcon.jsx
@@ -5,7 +5,7 @@ const LinkWithIcon = ({ title, link, emoji, sidebar }) => {
   return (
     <NavLink
       to={link}
-      className={sidebar ? "align_center sidebar_link" : "align_center"}
+      className={sidebar ? "align_center  sidebar_link" : "align_center"}
     >
       {title} <img src={emoji} alt="" className="link_emoji" />
     </NavLink>
